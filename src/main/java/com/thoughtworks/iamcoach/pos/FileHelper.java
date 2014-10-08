646fc7b41abb6e864c1ec7847540bbda6925985f
SnakerWorker <snakerworker@gmail.com>
2014-10-08 23:04:37 +0800

    add test get method

--- src/main/java/com/thoughtworks/iamcoach/pos/FileHelper.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/FileHelper.java
@@ -1,7 +1,12 @@
 package com.thoughtworks.iamcoach.pos;
 
+import java.util.List;
+
 /**
  * Created by sukai on 10/8/14.
  */
 public class FileHelper {
+    public List get(String s) {
+        return null;
+    }
 }
--- src/test/java/com/thoughtworks/iamcoach/pos/FileHelperTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/FileHelperTest.java
@@ -1,7 +1,23 @@
 package com.thoughtworks.iamcoach.pos;
 
-/**
- * Created by sukai on 10/8/14.
- */
+import org.junit.Test;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.List;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class FileHelperTest {
+    @Test
+    public void getTest() throws IOException {
+
+        FileHelper fileHelper = mock(FileHelper.class);
+        BufferedReader br = mock(BufferedReader.class);
+        when(br.readLine()).thenReturn("line1", "line2", "line3");
+
+        List list = fileHelper.get("cart.txt");
+        assertThat(list.size()).isEqualTo(9);
+    }
 }
