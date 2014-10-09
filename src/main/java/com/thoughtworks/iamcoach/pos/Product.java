c518ed1d5b4440dd132ce02be4bcbd319d54b534
SnakerWorker <snakerworker@gmail.com>
2014-10-09 12:57:28 +0800

    add field and get/set method

--- src/main/java/com/thoughtworks/iamcoach/pos/Product.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Product.java
@@ -1,7 +1,52 @@
 package com.thoughtworks.iamcoach.pos;
 
-/**
- * Created by sukai on 10/9/14.
- */
 public class Product {
+
+    private String barcode;
+    private String name;
+    private String unit;
+    private double price;
+
+    public Product() {
+
+    }
+
+    public Product(String barcode, String name, String unit, double price) {
+        this.barcode = barcode;
+        this.name = name;
+        this.unit = unit;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
