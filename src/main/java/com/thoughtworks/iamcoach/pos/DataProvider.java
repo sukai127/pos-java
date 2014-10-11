9965f16eb31717274959701855da1827d72a944f
SnakerWorker <snakerworker@gmail.com>
2014-10-11 13:55:20 +0800

    modify buildProduct

--- src/main/java/com/thoughtworks/iamcoach/pos/DataProvider.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/DataProvider.java
@@ -4,7 +4,12 @@ package com.thoughtworks.iamcoach.pos;
  * Created by sukai on 10/11/14.
  */
 public class DataProvider {
-    public Product buildProduct(String str) {
-        return null;
+    public Product buildProduct(String productString) {
+
+        String []fields = productString.split(",");
+        double price = Double.parseDouble(fields[3]);
+
+        Product product = new Product(fields[0],fields[1],fields[2],price);
+        return product;
     }
 }
