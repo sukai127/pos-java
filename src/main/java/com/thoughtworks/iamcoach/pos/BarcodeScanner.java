c5948fa9961ef2de96776963be2358341dbb64ee
SnakerWorker <snakerworker@gmail.com>
2014-10-11 15:08:25 +0800

    add test should_get_a_product_when_input_an_barcode

---
+++ src/main/java/com/thoughtworks/iamcoach/pos/BarcodeScanner.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.iamcoach.pos;
+
+/**
+ * Created by sukai on 10/11/14.
+ */
+public class BarcodeScanner {
+    public Product getProduct(String barcode) {
+        return null;
+    }
+}
--- src/test/java/com/thoughtworks/iamcoach/pos/BarcodeScannerTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/BarcodeScannerTest.java
@@ -1,7 +1,20 @@
 package com.thoughtworks.iamcoach.pos;
 
-/**
- * Created by sukai on 10/11/14.
- */
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+
 public class BarcodeScannerTest {
+
+    @Test
+    public void should_get_a_product_when_input_an_barcode(){
+
+        BarcodeScanner barcodeScanner = new BarcodeScanner();
+        String barcode = "ITEM000001";
+        Product product = barcodeScanner.getProduct(barcode);
+
+        assertThat(product.getName()).isEqualTo("雪碧");
+        assertEquals(product.getName(),"雪碧");
+    }
 }
