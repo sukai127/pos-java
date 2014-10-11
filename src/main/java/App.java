3ae64d51bc7ec07dff0e823586dcc9845d1cf9a2
SnakerWorker <snakerworker@gmail.com>
2014-10-11 18:18:54 +0800

    modify main method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,20 @@
+import com.thoughtworks.iamcoach.pos.BarcodeScanner;
+import com.thoughtworks.iamcoach.pos.CartItem;
+import com.thoughtworks.iamcoach.pos.FileHelper;
+
+import java.io.IOException;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-        //TODO: Need to implement.
+        try {
+            List list = FileHelper.get("cart.txt");
+            BarcodeScanner barcodeScanner = new BarcodeScanner();
+            List<CartItem> cartItemList = barcodeScanner.scan(list);
+            System.out.print("******let us go购物清单*******");
+
+        } catch (IOException e) {
+            System.out.println("对不起本机暂停服务。。。。。。。。。。");
+        }
     }
 }
