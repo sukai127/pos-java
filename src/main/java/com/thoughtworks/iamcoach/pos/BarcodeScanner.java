3ca7068b6a95d0765fa25c98637ec58050efdfde
SnakerWorker <snakerworker@gmail.com>
2014-10-11 15:18:50 +0800

    modify getProduct

--- src/main/java/com/thoughtworks/iamcoach/pos/BarcodeScanner.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/BarcodeScanner.java
@@ -1,10 +1,19 @@
 package com.thoughtworks.iamcoach.pos;
 
-/**
- * Created by sukai on 10/11/14.
- */
+import java.io.IOException;
+import java.util.List;
+
 public class BarcodeScanner {
-    public Product getProduct(String barcode) {
+    public Product getProduct(String barcode) throws IOException {
+
+        DataProvider dataProvider = new DataProvider();
+        List<Product> list = dataProvider.getProductList();
+
+        for(Product product : list){
+            if(product.getBarcode().equals(barcode)){
+                return product;
+            }
+        }
         return null;
     }
 }
--- src/test/java/com/thoughtworks/iamcoach/pos/BarcodeScannerTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/BarcodeScannerTest.java
@@ -2,13 +2,15 @@ package com.thoughtworks.iamcoach.pos;
 
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
 public class BarcodeScannerTest {
 
     @Test
-    public void should_get_a_product_when_input_an_barcode(){
+    public void should_get_a_product_when_input_an_barcode() throws IOException {
 
         BarcodeScanner barcodeScanner = new BarcodeScanner();
         String barcode = "ITEM000001";
