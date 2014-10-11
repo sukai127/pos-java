c356c3e2934df0d2c71fec6d4f1cc938efc84236
SnakerWorker <snakerworker@gmail.com>
2014-10-11 13:19:53 +0800

    add test should_return_an_product_object

---
+++ src/main/java/com/thoughtworks/iamcoach/pos/DataProvider.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.iamcoach.pos;
+
+/**
+ * Created by sukai on 10/11/14.
+ */
+public class DataProvider {
+    public Product buildProduct(String str) {
+        return null;
+    }
+}
--- src/test/java/com/thoughtworks/iamcoach/pos/DataProviderTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/DataProviderTest.java
@@ -1,7 +1,17 @@
 package com.thoughtworks.iamcoach.pos;
 
-/**
- * Created by sukai on 10/11/14.
- */
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class DataProviderTest {
+    @Test
+    public void should_return_an_product_object(){
+
+        DataProvider dp = new DataProvider();
+        String str = "ITEM000005,方便面,袋,4.50";
+
+        Product product = dp.buildProduct(str);
+        assertThat(product.getName()).isEqualTo("方便面");
+    }
 }
