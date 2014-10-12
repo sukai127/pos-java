704e6db7296ced0b043efbf83859f073dd4356aa
SnakerWorker <snakerworker@gmail.com>
2014-10-12 17:10:45 +0800

    override getMoney

--- src/main/java/com/thoughtworks/iamcoach/pos/SecondHalfPricePromotion.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/SecondHalfPricePromotion.java
@@ -4,6 +4,12 @@ public class SecondHalfPricePromotion extends Promotion {
 
     @Override
     public double getMoney(CartItem cartItem) {
-        return 0;
+
+        int promotionCount = (int)(cartItem.getCount()/2.0);
+        double count = cartItem.getCount() - promotionCount;
+        double promotionMoney = cartItem.getProduct().getPrice()/2 * promotionCount;
+        double money = cartItem.getProduct().getPrice() * count;
+
+        return money + promotionMoney;
     }
 }
