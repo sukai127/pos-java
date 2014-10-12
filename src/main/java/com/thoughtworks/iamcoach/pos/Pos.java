207bc8bb0e997d300d3c4323e36002c0cae804e4
SnakerWorker <snakerworker@gmail.com>
2014-10-12 21:16:39 +0800

    add fields and methods

--- src/main/java/com/thoughtworks/iamcoach/pos/Pos.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Pos.java
@@ -1,7 +1,30 @@
 package com.thoughtworks.iamcoach.pos;
 
-/**
- * Created by sukai on 10/12/14.
- */
 public class Pos {
+    private Scanner scanner;
+    private PromotionHelper promotionHelper;
+
+    public Pos() {
+    }
+
+    public Pos(Scanner scanner, PromotionHelper promotionHelper) {
+        this.scanner = scanner;
+        this.promotionHelper = promotionHelper;
+    }
+
+    public Scanner getScanner() {
+        return scanner;
+    }
+
+    public void setScanner(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public PromotionHelper getPromotionHelper() {
+        return promotionHelper;
+    }
+
+    public void setPromotionHelper(PromotionHelper promotionHelper) {
+        this.promotionHelper = promotionHelper;
+    }
 }
