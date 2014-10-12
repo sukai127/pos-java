1bc8bf8df2c4a249ac7e070cc75460d31c079d6d
SnakerWorker <snakerworker@gmail.com>
2014-10-12 16:31:04 +0800

    add getMoney

--- src/main/java/com/thoughtworks/iamcoach/pos/Promotion.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Promotion.java
@@ -1,7 +1,11 @@
 package com.thoughtworks.iamcoach.pos;
 
-/**
- * Created by sukai on 10/12/14.
- */
-public class Promotion {
+public abstract class Promotion {
+    
+    public static final int BUY_TWO_GET_ONE = 0;
+    public static final int SECOND_HALF_PRICE = 1;
+    public static final int DISCOUNT = 2;
+
+    public abstract double getMoney(CartItem cartItem);
+
 }
