cf980672adbb1f5bd47930c8ddd85d762b1d4150
SnakerWorker <snakerworker@gmail.com>
2014-10-12 22:13:06 +0800

    add printInventory

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,19 +1,17 @@
-import com.thoughtworks.iamcoach.pos.BarcodeScanner;
-import com.thoughtworks.iamcoach.pos.CartItem;
-import com.thoughtworks.iamcoach.pos.FileHelper;
+import com.thoughtworks.iamcoach.pos.*;
 
-import java.io.IOException;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
         try {
             List list = FileHelper.get("cart.txt");
-            BarcodeScanner barcodeScanner = new BarcodeScanner();
-            List<CartItem> cartItemList = barcodeScanner.scan(list);
-            System.out.print("******let us go购物清单*******");
+            Scanner scanner = new BarcodeScanner();
+            PromotionHelper promotionHelper = new PromotionHelper();
+            Pos pos = new Pos(scanner,promotionHelper);
+            pos.printInventory(list);
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             System.out.println("对不起本机暂停服务。。。。。。。。。。");
         }
     }
--- src/main/java/com/thoughtworks/iamcoach/pos/Pos.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Pos.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.iamcoach.pos;
 
+import java.util.List;
+
 public class Pos {
     private Scanner scanner;
     private PromotionHelper promotionHelper;
@@ -26,5 +28,19 @@ public class Pos {
 
     public void setPromotionHelper(PromotionHelper promotionHelper) {
         this.promotionHelper = promotionHelper;
+    }
+
+    public void printInventory(List<String> barcodes) throws Exception {
+        List<CartItem> cartItemList = this.getScanner().scan(barcodes);
+        StringBuffer stringBuffer = new StringBuffer("\t\t\t\t\t let us go购物清单 \t\t\t\t\t\t\n\n");
+        stringBuffer.append("\t名称 \t\t单位 \t\t价格 \t\t数量 \t\t小计\n\n");
+        for(CartItem cartItem:cartItemList){
+            stringBuffer.append("\t"+cartItem.getProduct().getName()+" \t");
+            stringBuffer.append(" \t"+cartItem.getProduct().getUnit()+" \t\t");
+            stringBuffer.append(" \t"+cartItem.getProduct().getPrice()+" \t");
+            stringBuffer.append(" \t"+cartItem.getCount()+" \t");
+            stringBuffer.append(" \t"+this.getPromotionHelper().calculateMoney(cartItem)+"\n");
+        }
+        System.out.println(stringBuffer.toString());
     }
 }
