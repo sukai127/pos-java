a2dab6e840b9c6da236d241e965165697c20b5f7
SnakerWorker <snakerworker@gmail.com>
2014-10-12 22:20:43 +0800

    add getDate method

--- src/main/java/com/thoughtworks/iamcoach/pos/Pos.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Pos.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.iamcoach.pos;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class Pos {
@@ -31,9 +33,12 @@ public class Pos {
     }
 
     public void printInventory(List<String> barcodes) throws Exception {
+
         List<CartItem> cartItemList = this.getScanner().scan(barcodes);
         StringBuffer stringBuffer = new StringBuffer("\t\t\t\t\t let us go购物清单 \t\t\t\t\t\t\n\n");
+        stringBuffer.append("\t\t\t\t\t\t\t\t\t"+this.getDate()+"\n\n");
         stringBuffer.append("\t名称 \t\t单位 \t\t价格 \t\t数量 \t\t小计\n\n");
+
         for(CartItem cartItem:cartItemList){
             stringBuffer.append("\t"+cartItem.getProduct().getName()+" \t");
             stringBuffer.append(" \t"+cartItem.getProduct().getUnit()+" \t\t");
@@ -42,5 +47,11 @@ public class Pos {
             stringBuffer.append(" \t"+this.getPromotionHelper().calculateMoney(cartItem)+"\n");
         }
         System.out.println(stringBuffer.toString());
+    }
+
+    private String getDate(){
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return format.format(date);
     }
 }
