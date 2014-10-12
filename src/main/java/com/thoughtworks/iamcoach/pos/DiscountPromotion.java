912b56bd3184b4eeda9bec97c51dca9959051f72
SnakerWorker <snakerworker@gmail.com>
2014-10-12 17:40:26 +0800

    refactor DiscountPromotion

--- src/main/java/com/thoughtworks/iamcoach/pos/DiscountPromotion.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/DiscountPromotion.java
@@ -7,20 +7,27 @@ public class DiscountPromotion extends Promotion{
 
     @Override
     public double getMoney(CartItem cartItem) {
+
+        int discount = this.getDiscount(cartItem.getProduct().getBarcode());
+        double money = cartItem.getProduct().getPrice() * cartItem.getCount() * discount / 100.0;
+        return money;
+
+    }
+
+    private int getDiscount(String barcode){
         List<String> list;
+        int discount = 100;
+
         try {
             list = FileHelper.get("discount_promotion.txt");
-            int discount = 100;
+
             for(String str : list){
-                if(str.contains(cartItem.getProduct().getBarcode())){
+                if(str.contains(barcode)){
                     discount = Integer.parseInt(str.split(":")[1]);
                 }
             }
-            double money = cartItem.getProduct().getPrice() * cartItem.getCount() * discount / 100.0;
-            return money;
         } catch (IOException e) {
-            e.printStackTrace();
         }
-        return cartItem.getProduct().getPrice() * cartItem.getCount();
+        return discount;
     }
 }
