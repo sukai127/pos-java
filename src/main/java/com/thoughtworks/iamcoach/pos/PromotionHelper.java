fc24eed64a39e4a91be6667f9ba02049a1715765
SnakerWorker <snakerworker@gmail.com>
2014-10-12 20:31:42 +0800

    add test should_return_12_when_input_cartItem

---
+++ src/main/java/com/thoughtworks/iamcoach/pos/PromotionHelper.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.iamcoach.pos;
+
+/**
+ * Created by sukai on 10/12/14.
+ */
+public class PromotionHelper {
+
+    public double calculateMoney(CartItem cartItem) {
+        return 0;
+    }
+}
--- src/test/java/com/thoughtworks/iamcoach/pos/PromotionHelperTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/PromotionHelperTest.java
@@ -1,7 +1,30 @@
 package com.thoughtworks.iamcoach.pos;
 
-/**
- * Created by sukai on 10/12/14.
- */
+import org.junit.Test;
+
+import javax.print.attribute.IntegerSyntax;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class PromotionHelperTest {
+    @Test
+    public void should_return_12_when_input_cartItem(){
+
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(0);
+        list.add(1);
+        list.add(2);
+
+        Product product = new Product("ITEM000000","可乐","瓶",3.00,list);
+        CartItem cartItem = new CartItem(product,6);
+        PromotionHelper  promotionHelper = new PromotionHelper();
+
+        double money = promotionHelper.calculateMoney(cartItem);
+
+        assertThat(money).isEqualTo(12);
+    }
+
 }
