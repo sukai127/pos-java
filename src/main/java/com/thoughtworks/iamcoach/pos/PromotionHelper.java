c1b0274d1ee869c98325c26f2e1123646a4434f3
SnakerWorker <snakerworker@gmail.com>
2014-10-12 21:37:00 +0800

    modify calculateMoney

--- src/main/java/com/thoughtworks/iamcoach/pos/PromotionHelper.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/PromotionHelper.java
@@ -7,6 +7,7 @@ public class PromotionHelper {
 
     public double calculateMoney(CartItem cartItem) {
         List<Integer> list = cartItem.getProduct().getPromotionTypes();
+        double result = cartItem.getProduct().getPrice() * cartItem.getCount();
         double money[] = new double[list.size()];
         Promotion promotion = null;
         for (int i = 0; i < list.size(); i++) {
@@ -16,7 +17,7 @@ public class PromotionHelper {
         }
         Arrays.sort(money);
 
-        return money[0];
+        return money.length == 0 ? result : money[0];
     }
 
 
