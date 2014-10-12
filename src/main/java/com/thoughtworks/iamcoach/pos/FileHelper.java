4cb69e774a1a4ba2625269f389c9239b8d0ebf51
SnakerWorker <snakerworker@gmail.com>
2014-10-12 17:55:06 +0800

    modify get method

--- src/main/java/com/thoughtworks/iamcoach/pos/FileHelper.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/FileHelper.java
@@ -1,24 +1,18 @@
 package com.thoughtworks.iamcoach.pos;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 public class FileHelper {
 
     public static List get (String filename) throws IOException {
 
-        List<String> list = new ArrayList<String>();
-        String path = FileHelper.class.getClassLoader().getResource(filename).getPath();
+        String filepath = FileHelper.class.getClassLoader().getResource(filename).getPath();
+        Path path = FileSystems.getDefault().getPath(filepath);
 
-        BufferedReader br = new BufferedReader(new FileReader(path));
-
-        String str;
-        while((str = br.readLine()) != null){
-            list.add(str);
-        }
-        return list;
+        return Files.readAllLines(path);
     }
 }
