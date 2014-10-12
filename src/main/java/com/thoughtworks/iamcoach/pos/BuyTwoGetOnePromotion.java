4f604af537a8f03f7de9aec0d616ad2abe8225f0
SnakerWorker <snakerworker@gmail.com>
2014-10-12 16:32:24 +0800

    add BuyTwoGetOnePromotion

---
+++ src/main/java/com/thoughtworks/iamcoach/pos/BuyTwoGetOnePromotion.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.iamcoach.pos;
+
+/**
+ * Created by sukai on 10/12/14.
+ */
+public class BuyTwoGetOnePromotion {
+}
--- src/main/java/com/thoughtworks/iamcoach/pos/Promotion.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Promotion.java
@@ -1,7 +1,7 @@
 package com.thoughtworks.iamcoach.pos;
 
 public abstract class Promotion {
-    
+
     public static final int BUY_TWO_GET_ONE = 0;
     public static final int SECOND_HALF_PRICE = 1;
     public static final int DISCOUNT = 2;
