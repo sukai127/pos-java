3fe44475daa50350137bff1264ec2612bd764dc9
SnakerWorker <snakerworker@gmail.com>
2014-10-12 16:48:26 +0800

    add test should_return_12_when_input_cartItem

--- src/test/java/com/thoughtworks/iamcoach/pos/BuyTwoGetOnePromotionTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/BuyTwoGetOnePromotionTest.java
@@ -1,7 +1,20 @@
 package com.thoughtworks.iamcoach.pos;
 
-/**
- * Created by sukai on 10/12/14.
- */
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class BuyTwoGetOnePromotionTest {
+
+    @Test
+    public void should_return_12_when_input_cartItem(){
+
+        Product product = new Product("ITEM000000","可乐","瓶",3.00);
+        CartItem cartItem = new CartItem(product,5);
+        BuyTwoGetOnePromotion buyTwoGetOnePromotion = new BuyTwoGetOnePromotion();
+
+        double money = buyTwoGetOnePromotion.getMoney(cartItem);
+
+        assertThat(money).isEqualTo(12);
+    }
 }
