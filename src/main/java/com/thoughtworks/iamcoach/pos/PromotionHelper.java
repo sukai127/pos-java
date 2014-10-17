189dc3af6abc436ade4317cce1230922e1ce3c9a
SnakerWorker <snakerworker@gmail.com>
2014-10-17 20:27:35 +0800

    format PromotionHelper

--- src/main/java/com/thoughtworks/iamcoach/pos/PromotionHelper.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/PromotionHelper.java
@@ -6,10 +6,12 @@ import java.util.List;
 public class PromotionHelper {
 
     public double calculateMoney(CartItem cartItem) {
+
         List<Integer> list = cartItem.getProduct().getPromotionTypes();
         double result = cartItem.getProduct().getPrice() * cartItem.getCount();
         double money[] = new double[list.size()];
         Promotion promotion = null;
+
         for (int i = 0; i < list.size(); i++) {
             promotion = Promotion.getPromotion(list.get(i));
             double subTotal = promotion.getMoney(cartItem);
